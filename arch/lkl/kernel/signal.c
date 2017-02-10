12ea48f2797a423c090aae106b9e1432672b0642
Hajime Tazaki <thehajime@gmail.com>
2017-02-10 17:33:28 +0900

    lkl: add signal delivery support
    This only handles signals right after a system call was invoked.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

---
+++ arch/lkl/kernel/signal.c
@@ -0,0 +1,16 @@
+#include <linux/sched.h>
+
+static void handle_signal(struct ksignal *ksig, struct pt_regs *regs)
+{
+	ksig->ka.sa.sa_handler(ksig->sig);
+}
+
+void do_signal(struct pt_regs *regs)
+{
+	struct ksignal ksig;
+
+	while (get_signal(&ksig)) {
+		/* Whee!  Actually deliver the signal.  */
+		handle_signal(&ksig, regs);
+	}
+}
