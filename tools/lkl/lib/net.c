82f73f9dc65753c5f02aa98afda298a3263520a8
Hajime Tazaki <thehajime@gmail.com>
2017-02-11 23:37:47 +0900

    lkl: add rump hypercall implementation for frankenlibc build
    This commit adds an implementation of new host_ops by using rump
    hypercall (wrapped as an LKL host_ops) provided by rumpkernel project.
    The rump hypercall implementation is specifically integrated with the
    one provided by frankenlibc project, which is an alternative way to use
    rump hypercalls.
    
    There are other rump hypercall implementations such as rumprun, NetBSD
    kernel tree, but this commit only focuses on the frankenlibc
    integration.
    
    The new features covered by this commits ranges in various ways, but
    here is some highlights.
    
    - New thread primitives
      frankenlibc implements user-space thread implementation based on
      makecontext(3)&co with non-preemptive (cooperative) scheduler
      implementation.
    - LKL specific standard library bind
      frankenlibc offers cross-compiler build chains to build an LKL
      application bound to LKL-versioned standard library (i.e., libc). Our
      musl libc implementation is provided by different repository.
      So, any application can be built with this toolchain to use LKL
      instead of host libc.
    - New host environments support
      LKLed application now has potentials to run on the platform which rump
      hypercall implementations support.  frankenlibc supports Liunx,
      FreeBSD, NetBSD, and qemu-arm.  Others supposrts run an LKL
      applications on hypervisors (kvm, xen, etc)
    
    The frankenlibc code is located at the following repository, but it will
    be upstreamed to the original one (justincormack/frankenlibc).
    
    https://github.com/libos-nuse/frankenlibc
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- tools/lkl/lib/net.c
+++ tools/lkl/lib/net.c
@@ -1,7 +1,11 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #include "endian.h"
 #include <lkl_host.h>
+
+#ifdef RUMPUSER
+#include "rump.h"
+#endif
 
 static inline void set_sockaddr(struct lkl_sockaddr_in *sin, unsigned int addr,
 				unsigned short port)
---
+++ tools/lkl/lib/rump.h
@@ -0,0 +1,20 @@
+/*
+ * Rump hypercall interface for Linux
+ * Copyright (c) 2015 Hajime Tazaki
+ *
+ * Author: Hajime Tazaki <thehajime@gmail.com>
+ */
+
+#define __dead
+#define __printflike(x, y)
+#include <rump/rumpuser.h>
+
+struct irq_data;
+
+void rump_sysproxy_init(void);
+void rump_sysproxy_fini(void);
+
+extern const struct rumpuser_hyperup hyp;
+
+int rump_pci_irq_request(struct irq_data *data);
+void rump_pci_irq_release(struct irq_data *data);
